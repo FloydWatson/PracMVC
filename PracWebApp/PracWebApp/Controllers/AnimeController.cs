76043eb8816a51df588bdefede49d11e3848af43
Shannon Roberts <sneakypuppers@gmail.com>
2019-09-05 17:32:20 +1200

    Added Anime Model, Anime Controller, AddAnime View, Index View

---
+++ PracWebApp/PracWebApp/Controllers/AnimeController.cs
@@ -0,0 +1,40 @@
+﻿using PracWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracWebApp.Controllers
+{
+    public class AnimeController : Controller
+    {
+        // GET: Anime
+        static List<AnimeModel> AnimeList = new List<AnimeModel>();
+        public ActionResult Index()
+        {
+            return View(AnimeList);
+        }
+
+        public ActionResult AddAnime()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAnime(AnimeModel a)
+        {
+            if (ModelState.IsValid)
+            {
+                AnimeList.Add(a);
+                return RedirectToAction("Index");
+            };
+
+            return View("AddAnime");
+            
+            
+        }
+
+        
+    }
+}
---
+++ PracWebApp/PracWebApp/Models/AnimeModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PracWebApp.Models
+{
+    public class AnimeModel
+    {
+        [Required]
+        public string AnimeName { get; set; }
+
+        [Required]
+        public string Genre1 { get; set; }
+
+        public string Genre2 { get; set; }
+
+        [Required]
+        public int SeasonCount { get; set; }
+
+        [Range(1,9999)]
+        public int EpisodeCount { get; set; }
+    }
+}
