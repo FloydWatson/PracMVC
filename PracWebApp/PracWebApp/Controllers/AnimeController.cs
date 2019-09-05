ef61a8feec778dd986d83ade13873891f8423d9a
Shannon Roberts <sneakypuppers@gmail.com>
2019-09-05 17:42:53 +1200

    Added Anime View to Nav, Added anime in list

--- PracWebApp/PracWebApp/Controllers/AnimeController.cs
+++ PracWebApp/PracWebApp/Controllers/AnimeController.cs
@@ -13,6 +13,10 @@ namespace PracWebApp.Controllers
         static List<AnimeModel> AnimeList = new List<AnimeModel>();
         public ActionResult Index()
         {
+            AnimeList.Add(new AnimeModel { AnimeName = "Hunter X Hunter", Genre1 = "Adventure", Genre2 = "Action", SeasonCount = 1, EpisodeCount = 146 });
+            AnimeList.Add(new AnimeModel { AnimeName = "Food Wars", Genre1 = "Comedy", Genre2 = "School", SeasonCount = 3, EpisodeCount = 48 });
+            AnimeList.Add(new AnimeModel { AnimeName = "The Devil Is A Part Timer", Genre1 = "Comedy", Genre2 = "Action", SeasonCount = 1, EpisodeCount = 13 });
+
             return View(AnimeList);
         }
 
